40b79973ce71070ad10f8ab716ab48fbd4aadedf
Trainy32 <cjstkdthsu98@naver.com>
2022-06-26 19:50:20 +0900

    PWA 적용해서 셋팅...!

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBd30-qOfY-BvFZZokzweAUXGmQSklOVoM",
+  authDomain: "pwa-prac-f3b88.firebaseapp.com",
+  projectId: "pwa-prac-f3b88",
+  storageBucket: "pwa-prac-f3b88.appspot.com",
+  messagingSenderId: "162726303497",
+  appId: "1:162726303497:web:e38a69e6e38de8c73965d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
